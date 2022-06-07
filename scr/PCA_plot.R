ef9135deac1c1f581c4ed057da8cc1b446fe2205
SissiB <buonaiutosilvia@gmail.com>
2022-06-07 13:15:36 +0200

    update pca plot script

--- scr/PCA_plot.R
+++ scr/PCA_plot.R
@@ -15,7 +15,20 @@ all <-rbind(all,myd)
 }
 
 all$chrom<-factor(all$chrom, levels=c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"))
-all$length<- factor(all$length, levels=c("whole_chromosome", "p_arm", "q_arm"))
-mycol<-c("#FF6B6B", "#FFD93D", "#6BCB77", "#4D96FF")
-all %>% mutate(acro = ifelse(chrom == "chr13" | chrom == "chr14" | chrom == "chr15" | chrom == "chr21" | chrom == "chr22", "yes", "no")) %>% filter(method == paste(args[2])) %>% ggplot(aes(PC1, PC2, color = Superpopulation)) + geom_point(size = 1, alpha = 0.6) + facet_nested(chrom~length + assembly) + theme_bw() + theme(legend.text=element_text(size=13), axis.text.x=element_text(size=9), axis.text.y = element_text(size = 9) ,axis.title=element_text(size = 12), strip.text.x = element_text(size = 13), strip.text.y = element_text(size = 12) ,legend.title=element_blank()) + scale_color_manual(values = mycol)
-ggsave(args[3], width = 8, heigh = 25)
+all$length<- factor(all$length, levels=c("whole_chromosome", "q_arm", "p_arm"))
+mycol<-c("#FBCB0A", "#F32424", "#5FD068", "#612897")
+p1<-all %>% mutate(acro = ifelse(chrom == "chr13" | chrom == "chr14" | chrom == "chr15" | chrom == "chr21" | chrom == "chr22", "yes", "no")) %>% filter(method == paste(args[2])) %>% filter(chrom == "chr1" | chrom == "chr2"| chrom =="chr3"| chrom=="chr4"| chrom=="chr5" |chrom=="chr6"| chrom=="chr7"| chrom=="chr8"| chrom=="chr9"| chrom=="chr10"| chrom=="chr11") ggplot(aes(PC1, PC2, color = Superpopulation)) + geom_point(size = 1, alpha = 0.6) + facet_nested(chrom~length + assembly) + theme_bw() + theme(legend.text=element_text(size=13), axis.text.x=element_text(size=9), axis.text.y = element_text(size = 9) ,axis.title=element_text(size = 12), strip.text.x = element_text(size = 10), strip.text.y = element_text(size = 11) ,legend.title=element_blank()) + scale_color_manual(values = mycol) + xlim(-0.6, 0.2)
+p2<-all %>% mutate(acro = ifelse(chrom == "chr13" | chrom == "chr14" | chrom == "chr15" | chrom == "chr21" | chrom == "chr22", "yes", "no")) %>% filter(method == "PGGB") %>% filter(chrom == "chr12"| chrom =="chr13"| chrom=="chr14"| chrom=="chr15" |chrom=="chr16"| chrom=="chr17"| chrom=="chr18"| chrom=="chr19"| chrom=="chr20"| chrom=="chr21"| chrom == "chr22") %>% ggplot(aes(PC1, PC2, color = Superpopulation)) + geom_point(size = 1, alpha = 0.6) + facet_nested(chrom~length + assembly) + theme_bw() + theme(legend.text=element_text(size=13), axis.text.x=element_text(size=9), axis.text.y = element_text(size = 9) ,axis.title=element_text(size = 12), strip.text.x = element_text(size = 10), strip.text.y = element_text(size = 11) ,legend.title=element_blank()) + scale_color_manual(values = mycol) + guides(color = guide_legend(direction = "horizontal")) +  xlim(-0.6, 0.2)
+
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+mylegend<-g_legend(p2)
+p3 <- grid.arrange(arrangeGrob(p1 + theme(legend.position="none"),
+                         p2 + theme(legend.position="none"),
+                         nrow=1),
+             mylegend,nrow = 2, heights=c(10, 1))
+
+ggsave(args[3], plot = p3, width = 12, heigh = 13)
